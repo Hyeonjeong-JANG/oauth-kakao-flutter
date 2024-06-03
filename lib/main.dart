4c70e8920e6f177e6d8e11efd7afa729c135f134
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-06-03 17:52:23 +0900

    사용자 정보 가져오기 전까지 함

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: 'e5935e6595d0812e9e9c671705c3cdae'
+  );
   runApp(ProviderScope(child: MyApp()));
 }
 
--- lib/ui/login_page/login_page.dart
+++ lib/ui/login_page/login_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Login Page"),
+    return Scaffold(
+      appBar: AppBar(title: Text("카카오 로그인"),),
+      body: ElevatedButton(
+        onPressed: () async{
+          KakaoLogin();
+        },
+        child: Text("카카오 로그인"),
+      ),
     );
+  }
+  void KakaoLogin() async{
+    try {
+      OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+      print('카카오톡으로 로그인 성공 ${token.accessToken}');
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+    }
   }
 }
